73fc57c27870ee3556d585888b446ba937a6a4a6
无聊过客 <986771570@qq.com>
2020-06-09 23:37:27 +0800

    [add] 增加system module

--- eureka/src/main/java/com/xuyuanfeng/course/course/EurekaApplication.java
+++ eureka/src/main/java/com/xuyuanfeng/course/course/EurekaApplication.java
@@ -1,9 +1,13 @@
 package com.xuyuanfeng.course.course;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
+@Slf4j
 public class EurekaApplication {
 
     public static void main(String[] args) {
